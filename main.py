61503e07905d3717e1686c03e0f0842a3297b92a
Dutchban <daniel.mayer22@gmail.com>
2021-01-18 17:58:09 +0100

    With working email now

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-### This is an email slicer. It slices emails ###
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 while 1:
     provided_email = input('Please enter the emailadress i should slide for you...\n')
     position_ad = provided_email.find('@')
@@ -6,5 +9,32 @@ while 1:
     print('Part 1 is ' + sliced_part_1)
     sliced_part_2 = provided_email[position_ad+1:]
     print('Part 2 is ' + sliced_part_2)
+    action = input('Now that i sliced it, should we send an email to the adress? Y/N')
+    if action == "Y":
+        print("Okay")
+        mail_content = '''Hello,
+        This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
+        Thank You'''
+        #The mail addresses and password
+        sender_address = 'peterzwegertinthehood@gmail.com'
+        sender_pass = 'thisiszwegert!'
+        receiver_address = 'daniel.mayer22@gmail.com'
+        #Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+        #The body and the attachments for the mail
+        message.attach(MIMEText(mail_content, 'plain'))
+        #Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        session.starttls() #enable security
+        session.login(sender_address, sender_pass) #login with mail_id and password
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+        print('Mail Sent')
+    else:
+        print("Then not")
 
 
